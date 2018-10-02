9f239b61bd791283829f5832110c13a0cdc690be
Eduardo Almeida <eqalmeida@gmail.com>
2018-10-01 21:24:35 -0300

    Add gui lib file

--- src/br/com/caelum/contas/modelo/ContaCorrente.java
+++ src/br/com/caelum/contas/modelo/ContaCorrente.java
@@ -4,7 +4,11 @@ public class ContaCorrente extends Conta {
 	public String getTipo(){
 		return super.getTipo() + " Corrente";
 	}
-	
+
+	/**
+	 * Metodo saca da conta, com uma taxa
+	 * de 0,10 a cada transação.
+	 */
 	@Override
 	public void saca(double valor) {
 		super.saca((valor + 0.10));
